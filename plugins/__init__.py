a8a03b4b258c692cea9052112035e63dbcb672a9
kannadarockers <114350032+kannadarockers@users.noreply.github.com>
2026-07-04 12:43:19 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5,7 +5,6 @@ from .studio import studio_routes
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
 
-    web_app.add_routes(routes)
     web_app.add_routes(studio_routes)
-
+    web_app.add_routes(routes)
     return web_app
