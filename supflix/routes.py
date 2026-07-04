ff4852fe1114d574e406884a56f6c0b398f84505
kannadarockers <114350032+kannadarockers@users.noreply.github.com>
2026-07-04 16:38:40 +0530

    Started SUPFLIX frontend

--- supflix/routes.py
+++ supflix/routes.py
@@ -1,14 +1,20 @@
 from aiohttp import web
 from .template_engine import env
+import traceback
 
 supflix_routes = web.RouteTableDef()
 
 @supflix_routes.get("/supflix")
 async def supflix_home(request):
-    template = env.get_template("home.html")
-    html = template.render()
-
-    return web.Response(
-        text=html,
-        content_type="text/html"
-    )
+    try:
+        template = env.get_template("home.html")
+        html = template.render()
+        return web.Response(
+            text=html,
+            content_type="text/html"
+        )
+    except Exception:
+        return web.Response(
+            text=traceback.format_exc(),
+            content_type="text/plain"
+        )
